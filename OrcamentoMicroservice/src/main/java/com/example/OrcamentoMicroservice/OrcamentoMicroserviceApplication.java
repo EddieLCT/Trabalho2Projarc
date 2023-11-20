6c9ac9c10e064ac69a2afeafa8a77f7cc205344b
VictorHugoPUCRS <Victor.Boeira@edu.pucrs.br>
2023-11-20 18:11:48 -0300

    Ajeitando algumas coisas

---
+++ OrcamentoMicroservice/src/main/java/com/example/OrcamentoMicroservice/OrcamentoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrcamentoMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoMicroserviceApplication.class, args);
+	}
+
+}
---
+++ OrcamentoMicroservice/src/test/java/com/example/OrcamentoMicroservice/OrcamentoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrcamentoMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
